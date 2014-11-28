fb877b317caec82c37a8bbbbe7624697baae9969
Benjamin Weber <benjamin.weber@rogers.com>
2014-11-28 18:21:29 -0500

    Update install.php

--- install.php
+++ install.php
@@ -1,15 +1,22 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="css/common.css" />
+</head>
+<body style="background-color: gray">
+	<?php include 'header.html';?>
+</body>
 <?php
 require_once 'classes/Database.php';
 
 $db = new Database("");
-$db->create('sysc4504');
-//if($db->getError()) {
-//	http_response_code(503); // Service is unavailable
-//	echo "<p>The database service is currently unavailable. Please try again</p>";
-//	exit();
-//}
+if($db->getConnError()) {
+	http_response_code(503); // Service is unavailable
+	echo "<h1>The database service is currently unavailable. Please try again</h1>";
+	exit();
+}
 
-$db2 = new Database("sysc4504");
+$db->create('sysc4504'); //Create the sysc4504 database
+
+$db2 = new Database("sysc4504"); //Connect to the sysc4504 database
 
 $dirContents = scandir('sql'); //Get an Array of the contents of the SQL folder.
 
@@ -21,7 +28,7 @@ foreach($dirContents as $toProcess) {
 	//Else do nothing
 }
 
-
+echo "<h1>Done importing sysc4504 database</h1>";
 
 
 ?>
