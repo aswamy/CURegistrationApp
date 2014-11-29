96b13cd5ac0ae07db937ec4ae52cb9bd626e4ad3
Denis Dionne <DDionne777@gmail.com>
2014-11-28 19:54:09 -0500

    added index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	/* For ease of marking we named all our PHP files
+
+	after the view's they correlate to in the marking scheme
+	We still need an index.php to be served as the default page,however
+	so we just include view1a.php */ 
+	include('view1a.php'); 
+?>
